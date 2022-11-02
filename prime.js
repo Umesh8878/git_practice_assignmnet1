9d9d3c87aa1b5a10d6d9aa01b6135e87a988a8d0
DishaGup <guptamansi201@gmail.com>
2022-11-02 14:12:32 +0530

    call the function

--- prime.js
+++ prime.js
@@ -1,11 +1,13 @@
 function prime(num){
-    let bag="";
-    for(let i=0; i<=num; i++){
-        if(i%2==0){
-            console.log("Prime");
+    let bag=0;
+    for(let i=1; i<=num; i++){
+        if(num%i==0){
+ bag++}}
+
+   if(bag<2){         console.log("Prime");
         }
         else{
             console.log("Not Prime");
         }
-    }
+    
 }
