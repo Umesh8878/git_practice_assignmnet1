f953891e409dd7dda0d24df4f56ea43add48ff7d
Umesh <yomyouronlinemart@gmail.com>
2022-11-02 13:49:07 +0530

    prime code added

---
+++ prime.js
@@ -0,0 +1,11 @@
+function prime(num){
+    let bag="";
+    for(let i=0; i<=num; i++){
+        if(i%2==0){
+            console.log("Prime");
+        }
+        else{
+            console.log("Not Prime");
+        }
+    }
+}
